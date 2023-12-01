2a975f1eaab32c840d85e35190670ba060d0079a
jyj <busy.joj@gmail.com>
2023-12-01 16:39:06 +0900

    add GA

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import ReactGA from "react-ga";
+import LandingPage from "./pages/LandingPage";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css';
-import LandingPage from './pages/LandingPage';
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
+ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
+ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='landing' element={<LandingPage />}></Route>
+        <Route path="landing" element={<LandingPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
